c887a57196b950a82a164e5fdef2ac348be7197b
ddot727 <ddot727@gmail.com>
2019-01-28 17:15:22 -0500

    inserted navigation anchors

--- js/index.js
+++ js/index.js
@@ -40,3 +40,13 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+//===============My code===============
+
+const navAnchors = document.querySelectorAll('a');
+navAnchors[0].textContent = siteContent.nav['nav-item-1'];
+navAnchors[1].textContent = siteContent.nav['nav-item-2'];
+navAnchors[2].textContent = siteContent.nav['nav-item-3'];
+navAnchors[3].textContent = siteContent.nav['nav-item-4'];
+navAnchors[4].textContent = siteContent.nav['nav-item-5'];
+navAnchors[5].textContent = siteContent.nav['nav-item-6'];
